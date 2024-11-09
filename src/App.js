a19b94e529c7b1c49f29806a52421b0047a22942
sisilialeonita1384 <sisilialeonitap@gmail.com>
2024-11-09 22:35:33 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import NavbarComponent from './components/NavbarComponent';
+
+
+function App() {
+  return (
+    <div className="App">
+      <NavbarComponent />
+      <h2>Halo</h2>
+    </div>
+  );
+}
+
+export default App;
